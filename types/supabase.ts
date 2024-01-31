a41eab4894a69830d3a652a85b19b6ac1537e73a
Drillan767 <jd.levarato@gmail.com>
2024-01-31 23:46:28 +0100

    Install dependencies + setup project

---
+++ types/supabase.ts
@@ -0,0 +1,219 @@
+export type Json =
+  | string
+  | number
+  | boolean
+  | null
+  | { [key: string]: Json | undefined }
+  | Json[]
+
+export interface Database {
+  public: {
+    Tables: {
+      boxes: {
+        Row: {
+          created_at: string
+          id: number
+          name: string
+          occurrence: number
+          order: number
+        }
+        Insert: {
+          created_at?: string
+          id?: number
+          name: string
+          occurrence: number
+          order: number
+        }
+        Update: {
+          created_at?: string
+          id?: number
+          name?: string
+          occurrence?: number
+          order?: number
+        }
+        Relationships: []
+      }
+      cards: {
+        Row: {
+          answer: string
+          box_id: number | null
+          created_at: string
+          id: number
+          last_answered_at: string
+          media: string | null
+          notes: string | null
+          question: string
+          question_type: Database["public"]["Enums"]["question_types"]
+          tags: string | null
+        }
+        Insert: {
+          answer: string
+          box_id?: number | null
+          created_at?: string
+          id?: number
+          last_answered_at: string
+          media?: string | null
+          notes?: string | null
+          question: string
+          question_type?: Database["public"]["Enums"]["question_types"]
+          tags?: string | null
+        }
+        Update: {
+          answer?: string
+          box_id?: number | null
+          created_at?: string
+          id?: number
+          last_answered_at?: string
+          media?: string | null
+          notes?: string | null
+          question?: string
+          question_type?: Database["public"]["Enums"]["question_types"]
+          tags?: string | null
+        }
+        Relationships: [
+          {
+            foreignKeyName: "cards_box_id_fkey"
+            columns: ["box_id"]
+            isOneToOne: false
+            referencedRelation: "boxes"
+            referencedColumns: ["id"]
+          }
+        ]
+      }
+      statistics: {
+        Row: {
+          box_id: number
+          card_id: number
+          correct: boolean
+          created_at: string
+          difficulty: string
+          id: number
+        }
+        Insert: {
+          box_id: number
+          card_id: number
+          correct: boolean
+          created_at?: string
+          difficulty: string
+          id?: number
+        }
+        Update: {
+          box_id?: number
+          card_id?: number
+          correct?: boolean
+          created_at?: string
+          difficulty?: string
+          id?: number
+        }
+        Relationships: [
+          {
+            foreignKeyName: "statistics_box_id_fkey"
+            columns: ["box_id"]
+            isOneToOne: false
+            referencedRelation: "boxes"
+            referencedColumns: ["id"]
+          },
+          {
+            foreignKeyName: "statistics_card_id_fkey"
+            columns: ["card_id"]
+            isOneToOne: false
+            referencedRelation: "cards"
+            referencedColumns: ["id"]
+          }
+        ]
+      }
+    }
+    Views: {
+      [_ in never]: never
+    }
+    Functions: {
+      [_ in never]: never
+    }
+    Enums: {
+      question_types: "guess" | "exact" | "mcq"
+    }
+    CompositeTypes: {
+      [_ in never]: never
+    }
+  }
+}
+
+export type Tables<
+  PublicTableNameOrOptions extends
+    | keyof (Database["public"]["Tables"] & Database["public"]["Views"])
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
+        Database[PublicTableNameOrOptions["schema"]]["Views"])
+    : never = never
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
+      Database[PublicTableNameOrOptions["schema"]]["Views"])[TableName] extends {
+      Row: infer R
+    }
+    ? R
+    : never
+  : PublicTableNameOrOptions extends keyof (Database["public"]["Tables"] &
+      Database["public"]["Views"])
+  ? (Database["public"]["Tables"] &
+      Database["public"]["Views"])[PublicTableNameOrOptions] extends {
+      Row: infer R
+    }
+    ? R
+    : never
+  : never
+
+export type TablesInsert<
+  PublicTableNameOrOptions extends
+    | keyof Database["public"]["Tables"]
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
+    : never = never
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+      Insert: infer I
+    }
+    ? I
+    : never
+  : PublicTableNameOrOptions extends keyof Database["public"]["Tables"]
+  ? Database["public"]["Tables"][PublicTableNameOrOptions] extends {
+      Insert: infer I
+    }
+    ? I
+    : never
+  : never
+
+export type TablesUpdate<
+  PublicTableNameOrOptions extends
+    | keyof Database["public"]["Tables"]
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
+    : never = never
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+      Update: infer U
+    }
+    ? U
+    : never
+  : PublicTableNameOrOptions extends keyof Database["public"]["Tables"]
+  ? Database["public"]["Tables"][PublicTableNameOrOptions] extends {
+      Update: infer U
+    }
+    ? U
+    : never
+  : never
+
+export type Enums<
+  PublicEnumNameOrOptions extends
+    | keyof Database["public"]["Enums"]
+    | { schema: keyof Database },
+  EnumName extends PublicEnumNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicEnumNameOrOptions["schema"]]["Enums"]
+    : never = never
+> = PublicEnumNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicEnumNameOrOptions["schema"]]["Enums"][EnumName]
+  : PublicEnumNameOrOptions extends keyof Database["public"]["Enums"]
+  ? Database["public"]["Enums"][PublicEnumNameOrOptions]
+  : never
